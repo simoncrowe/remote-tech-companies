87889e5a9970bb21ba89e3ada49c12f3dd56f495
Simon Crowe <simon.r.crowe@protonmail.com>
2022-10-09 00:30:06 +0100

    Add source data

---
+++ company_data/grafana.py
@@ -0,0 +1,42 @@
+from company_data import RemotePolicy
+from helpers import scrape
+
+name = 'Grafana'
+business = 'Observability'
+
+
+def remote_policy():
+    '''Blog post about their remote-first culture'''
+    url = 'https://grafana.com/blog/2022/01/28/inside-grafana-labs-learn-about-our-remote-first-culture-and-meet-our-teams-at-our-virtual-open-house/'
+    return RemotePolicy.REMOTE_FIRST, url
+
+
+def hiring_region():
+    '''Listings for global and regional remote jobs'''
+    url = 'https://grafana.com/about/careers/open-positions/'
+    return 'global', url
+
+
+def engineering_glassdoor_rating():
+    '''Average glassdoor rating for engineers'''
+    url = 'https://www.glassdoor.co.uk/Reviews/Grafana-Labs-Engineering-Reviews-EI_IE2300269.0,12_DEPT1007.htm?filter.iso3Language=eng&filter.employmentStatus=REGULAR&filter.employmentStatus=PART_TIME'
+    return scrape.glassdoor_engineering_rating(url), url
+
+
+def overall_glassdoor_rating():
+    '''Overall average glassdoor rating'''
+    url = 'https://www.glassdoor.co.uk/Overview/Working-at-Grafana-Labs-EI_IE2300269.11,23.htm'
+    return scrape.glassdoor_rating(url), url
+
+
+def tech_stack():
+    '''Main languages from their open source repos'''
+    url = 'https://github.com/orgs/grafana/repositories'
+    tech = 'go, typescript'
+    return tech, url
+
+
+def salary():
+    '''Senior Software Engineer'''
+    url = 'https://www.levels.fyi/companies/grafana/salaries/software-engineer'
+    return scrape.levels_salary(url), url
---
+++ company_data/planet.py
@@ -0,0 +1,42 @@
+from company_data import RemotePolicy
+from helpers import scrape
+
+name = 'Planet'
+business = 'Satellite Imaging'
+
+
+def remote_policy():
+    '''Article about their remote hiring'''
+    url = 'https://www.planet.com/pulse/making-space-for-everyone-how-planet-is-building-a-global-workforce/'
+    return RemotePolicy.REMOTE_FRIENDLY, url
+
+
+def hiring_region():
+    '''Listings for regional remote jobs'''
+    url = 'https://www.planet.com/company/careers/'
+    return 'APAC, Americas, EMEA', url
+
+
+def engineering_glassdoor_rating():
+    '''Average glassdoor rating for engineers'''
+    url = 'https://www.glassdoor.co.uk/Reviews/Planet-Engineering-Reviews-EI_IE827495.0,6_DEPT1007.htm?filter.iso3Language=eng&filter.employmentStatus=REGULAR&filter.employmentStatus=PART_TIME'
+    return scrape.glassdoor_engineering_rating(url), url
+
+
+def overall_glassdoor_rating():
+    '''Overall average glassdoor rating'''
+    url = 'https://www.glassdoor.co.uk/Overview/Working-at-Planet-EI_IE827495.11,17.htm'
+    return scrape.glassdoor_rating(url), url
+
+
+def tech_stack():
+    '''A selection of the tech listed on himalayas.app'''
+    url = 'https://himalayas.app/companies/planet/tech-stack'
+    tech = 'js, python, go, c++, c, postgres, mysql, nginx,  ansible, terraform, k8s'
+    return tech, url
+
+
+def salary():
+    '''L4 Software Engineer in USA'''
+    url = 'https://www.levels.fyi/companies/planet/salaries/software-engineer'
+    return scrape.levels_salary(url), url
