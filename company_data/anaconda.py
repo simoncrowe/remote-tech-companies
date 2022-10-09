2dc27ebca91b3af0be18d2037ebdbcf09a3d7221
Simon Crowe <simon.r.crowe@protonmail.com>
2022-10-09 20:37:45 +0100

    Add Anaconda

---
+++ company_data/anaconda.py
@@ -0,0 +1,42 @@
+from company_data import RemotePolicy
+from helpers import scrape
+
+name = 'Anaconda'
+business = 'Data Science'
+
+
+def remote_policy():
+    '''Their career page, which lists remote jobs'''
+    url = 'https://www.anaconda.com/careers'
+    return RemotePolicy.REMOTE_FRIENDLY, url
+
+
+def hiring_region():
+    '''Listings for regional remote jobs'''
+    url = 'https://www.anaconda.com/careers#jobs'
+    return 'US, Canada, Germany, UK', url
+
+
+def engineering_glassdoor_rating():
+    '''Average glassdoor rating for engineers'''
+    url = 'https://www.glassdoor.co.uk/Reviews/Anaconda-Engineering-Reviews-EI_IE983617.0,8_DEPT1007.htm?filter.iso3Language=eng&filter.employmentStatus=REGULAR&filter.employmentStatus=PART_TIME'
+    return scrape.glassdoor_engineering_rating(url), url
+
+
+def overall_glassdoor_rating():
+    '''Overall average glassdoor rating'''
+    url = 'https://www.glassdoor.co.uk/Overview/Working-at-Anaconda-EI_IE983617.11,19.htm'
+    return scrape.glassdoor_rating(url), url
+
+
+def tech_stack():
+    '''Tech taken from Senior Full Stack Software Engineer job spec'''
+    url = 'https://boards.greenhouse.io/anaconda/jobs/4260312'
+    tech = 'python, javascript, mongodb, docker, k8s, fastapi'
+    return tech, url
+
+
+def salary():
+    '''Median Software Engineer salary'''
+    url = 'https://www.levels.fyi/companies/anaconda/salaries/software-engineer'
+    return scrape.levels_salary(url), url
