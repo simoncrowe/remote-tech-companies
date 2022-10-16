8cbc2affa86f0a5211bf3c2cc9c59f02343524b1
Simon Crowe <simon.r.crowe@protonmail.com>
2022-10-16 20:41:18 +0100

    Add InVision

---
+++ company_data/invision.py
@@ -0,0 +1,42 @@
+from company_data import RemotePolicy
+from helpers import scrape
+
+name = 'InVision'
+business = 'Visual Collaboration'
+
+
+def remote_policy():
+    '''Their careers page, which describes them as remote-first'''
+    url = 'https://www.invisionapp.com/careers'
+    return RemotePolicy.REMOTE_FIRST, url
+
+
+def hiring_region():
+    '''Listings for global remote jobs'''
+    url = 'https://www.invisionapp.com/careers'
+    return 'global', url
+
+
+def engineering_glassdoor_rating():
+    '''Average glassdoor rating for engineers'''
+    url = 'https://www.glassdoor.co.uk/Reviews/InVision-Engineering-Reviews-EI_IE912781.0,8_DEPT1007.htm?filter.iso3Language=eng&filter.employmentStatus=REGULAR&filter.employmentStatus=PART_TIME'
+    return scrape.glassdoor_engineering_rating(url), url
+
+
+def overall_glassdoor_rating():
+    '''Overall average glassdoor rating'''
+    url = 'https://www.glassdoor.co.uk/Overview/Working-at-InVision-EI_IE912781.11,19.htm'
+    return scrape.glassdoor_rating(url), url
+
+
+def tech_stack():
+    '''A selection of tech from stackshare.io'''
+    url = 'https://stackshare.io/invisionapp/invisionapp'
+    tech = 'js, react, angular, postgres, kafka, k8s, aws'
+    return tech, url
+
+
+def salary():
+    '''Senior Software Engineer based in UK'''
+    url = 'https://www.glassdoor.co.uk/Salary/InVision-Senior-Software-Engineer-Salaries-E912781_D_KO9,33.htm'
+    return scrape.glassdoor_salary(url), url
