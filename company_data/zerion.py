f8c8f08baaca5507fcbbc79d39e9b6a9167d47c9
Simon Crowe <simon.r.crowe@protonmail.com>
2022-11-18 20:21:06 +0000

    Add Zerion

---
+++ company_data/zerion.py
@@ -0,0 +1,40 @@
+from company_data import RemotePolicy
+from helpers import scrape
+
+name = 'Zerion'
+business = 'Crypto Wallet'
+
+
+def remote_policy():
+    '''A job spec, which describes the engineering team as fully remote'''
+    url = 'https://jobs.lever.co/zerion/6a66e7d7-440f-4fba-94f9-566cb4b7757e'
+    return RemotePolicy.REMOTE_FIRST, url
+
+
+def hiring_region():
+    '''Listings for regional and seemingly global remote jobs'''
+    url = 'https://jobs.lever.co/zerion'
+    return 'CIS, global?', url
+
+
+def overall_glassdoor_rating():
+    '''Overall average glassdoor rating'''
+    url = 'https://www.glassdoor.com/Overview/Working-at-Zerion-Software-EI_IE271496.11,26.htm'
+    return scrape.glassdoor_rating(url), url
+
+
+def engineering_glassdoor_rating():
+    '''Average glassdoor rating for engineers'''
+    url = 'https://www.glassdoor.co.uk/Reviews/Zerion-Engineering-Reviews-EI_IE3325248.0,6_DEPT1007.htm?filter.iso3Language=eng'
+    return scrape.glassdoor_engineering_rating(url), url
+
+
+def tech_stack():
+    '''Could not find any info online'''
+    return 'unknown', ''
+
+
+def salary():
+    '''Software Engineer based in USA'''
+    url = 'https://www.glassdoor.com/Salary/Zerion-Software-Software-Engineer-Salaries-E271496_DAO.htm?filter.jobTitleExact=Software+Engineer%28%29'
+    return scrape.glassdoor_salary(url), url
