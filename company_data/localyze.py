1ea5934905838cf4e003abbb89ab4e0238719673
Simon Crowe <simon.r.crowe@protonmail.com>
2022-12-16 21:33:56 +0000

    Add Localyze

---
+++ company_data/localyze.py
@@ -0,0 +1,35 @@
+from company_data import RemotePolicy
+from helpers import scrape
+
+name = 'Localyze'
+business = 'Remote HR'
+
+
+def remote_policy():
+    '''A blog post describing their remote culture'''
+    url = 'https://www.localyze.com/post/work-and-location-policy-at-localyze'
+    return RemotePolicy.REMOTE_FRIENDLY, url
+
+
+def hiring_region():
+    '''Listings for regional remote jobs'''
+    url = 'https://www.localyze.com/careers'
+    return 'Europe, Americas', url
+
+
+def overall_glassdoor_rating():
+    '''Overall average glassdoor rating'''
+    url = 'https://www.glassdoor.co.uk/Overview/Working-at-Localyze-EI_IE5377470.11,19.htm'
+    return scrape.glassdoor_rating(url), url
+
+
+def engineering_glassdoor_rating():
+    '''Average glassdoor rating for engineers'''
+    url = 'https://www.glassdoor.co.uk/Reviews/Localyze-Engineering-Reviews-EI_IE5377470.0,8_DEPT1007.htm?filter.iso3Language=eng'
+    return scrape.glassdoor_engineering_rating(url), url
+
+
+tech_stack = 'unknown'
+
+
+salary = 'unknown'
