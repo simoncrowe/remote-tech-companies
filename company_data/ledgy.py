1b6b9c47715931da1ea295a856a25c6b47e60d58
Simon Crowe <simon.r.crowe@protonmail.com>
2022-12-17 21:35:53 +0000

    Add Ledgy

---
+++ company_data/ledgy.py
@@ -0,0 +1,42 @@
+from company_data import RemotePolicy
+from helpers import scrape
+
+name = 'Ledgy'
+business = 'Fintech (Equity)'
+
+
+def remote_policy():
+    '''Their careers page, which mentions flexible and remote work'''
+    url = 'https://ledgy.com/uk/careers/'
+    return RemotePolicy.REMOTE_FIRST, url
+
+
+def hiring_region():
+    '''Listings for regional remote jobs'''
+    url = 'https://ledgy.com/uk/careers/'
+    return 'Europe', url
+
+
+def overall_glassdoor_rating():
+    '''Overall average glassdoor rating'''
+    url = 'https://www.glassdoor.co.uk/Overview/Working-at-Ledgy-EI_IE2406828.11,16.htm'
+    return scrape.glassdoor_rating(url), url
+
+
+def engineering_glassdoor_rating():
+    '''Average glassdoor rating for engineers'''
+    url = 'https://www.glassdoor.co.uk/Reviews/Ledgy-Engineering-Reviews-EI_IE2406828.0,5_DEPT1007.htm?filter.iso3Language=eng'
+    return scrape.glassdoor_engineering_rating(url), url
+
+
+def tech_stack():
+    '''Tech listed on Software Engineer job spec'''
+    url = 'https://ledgy.com/uk/careers/4000985101'
+    tech = 'js, typescript, mongodb, gcp'
+    return tech, url
+
+
+def salary():
+    '''Salary range on old Senior Software Engineer job spec'''
+    url = 'https://cord.co/u/ledgy/jobs/21087-senior-software-engineer-at-ledgy?%2Fjobs%2F10379-devops-lead-='
+    return '£80,000 - £105,000', url
