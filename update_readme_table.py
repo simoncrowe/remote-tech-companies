8fac411b6fe1cfc2ab93c996219626051d1baa26
Simon Crowe <simon.r.crowe@protonmail.com>
2022-12-20 21:58:36 +0000

    Prioritise shiny tech

--- update_readme_table.py
+++ update_readme_table.py
@@ -51,9 +51,9 @@ def sort_key(data):
         eng_rating >= 4.28 if eng_rating else True,  # Current employer
         overall_rating >= 3.78 if overall_rating else True,  # Current employer
         '[remote-first]' in data['remote_policy'],
+        any(tech in data['tech_stack'] for tech in desired_tech),
         eng_rating or overall_rating,
         overall_rating,
-        any(tech in data['tech_stack'] for tech in desired_tech),
     )
 
 
